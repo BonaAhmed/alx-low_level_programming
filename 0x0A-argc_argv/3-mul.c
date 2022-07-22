fccb79a895cce7b4cefd407dc28b614ad50c1cd4
BonaAhmed <you@examplc>
2022-07-22 08:00:54 -0700

    low level programing

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,24 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 
 /**
- *  * main - Multiplies two numbers
- *   * @argc: arguement count
- *    * @argv: arguement vector
- *     * Return: Always 1 (Fail)
- *      * Return: Always 0 (Success)
+ *  * whatsmyname - program that prints its name
+ *   * @argc: counts number of arguments on the command line
+ *    * @argv: array of pointer to the arguments passed(strings)
  */
-int main(int argc, char *argv[])
-{
-	int i, mul = 1;
 
-	if (argc > 1)
-	{
-		for (i = 1; i < argc; i++)
-			mul *= atoi(argv[i]);
-		printf("%d\n", mul);
-		return (0);
-	}
-	printf("Error\n");
-	return (1);
+int whatsmyname(int argc, char *argv[])
+{
+	return (0);
 }
